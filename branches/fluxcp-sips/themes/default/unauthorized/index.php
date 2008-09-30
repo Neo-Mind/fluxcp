f54c5ed28a8e7c3aa13a47038bd4a356ecd471ea
shugotenshi <shugotenshi@234f71bc-ae53-0410-a53c-012b120979b2>
2008-09-30 08:25:46 +0000

    Don't need this anymore.

--- branches/fluxcp-sips/themes/default/unauthorized/index.php
+++
@@ -1,3 +0,0 @@
-<?php if (!defined('FLUX_ROOT')) exit; ?>
-<h2 class="red">Unauthorized</h2>
-<p>You are unauthorized to view this page. <a href="<?php echo $metaRefresh['location'] ?>">Redirecting…</a></p>
