129eaf07eedf07578ad27e746165eb56643c43ee
Paradox924X <Paradox924X@234f71bc-ae53-0410-a53c-012b120979b2>
2013-01-30 07:00:26 +0000

    Prevented issues when attempting to use number_format() on a null value returned for guild tax where a record does not exist in the guild_position table.

--- branches/fluxcp-1.0/modules/guild/view.php
+++ branches/fluxcp-1.0/modules/guild/view.php
@@ -57,7 +57,7 @@ $col .= "WHEN DATE_FORMAT(DATE_SUB(NOW(), INTERVAL 1 DAY), '%Y-%m-%d') THEN 'Yes
 $col .= "ELSE CONCAT(DATEDIFF(NOW(), acc.lastlogin), ' Days Ago') ";
 $col .= "END) AS lastlogin, ";
 $col .= "roster.exp AS devotion, roster.position, ";
-$col .= "pos.name AS position_name, pos.mode, pos.exp_mode";
+$col .= "pos.name AS position_name, pos.mode, IFNULL(pos.exp_mode, 0) AS guild_tax";
 
 $sql  = "SELECT $col FROM {$server->charMapDatabase}.`char` AS ch ";
 $sql .= "LEFT JOIN {$server->loginDatabase}.login AS acc ON acc.account_id = ch.account_id ";
